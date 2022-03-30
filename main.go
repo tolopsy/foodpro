e7691987694e2ca129426a13a5dfecc087122222
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-03-31 00:37:21 +0100

    add delete recipe endpoint

--- main.go
+++ main.go
@@ -68,6 +68,30 @@ func UpdateRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes[index])
 }
 
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted",
+	})
+}
+
 func init() {
 	recipes = make([]Recipe, 0)
 	file, _ := ioutil.ReadFile("recipes.json")
@@ -79,5 +103,6 @@ func main() {
 	engine.POST("/recipes", CreateNewRecipeHandler)
 	engine.GET("/recipes", FetchAllRecipesHandler)
 	engine.PUT("/recipes/:id", UpdateRecipeHandler)
+	engine.DELETE("/recipes/:id", DeleteRecipeHandler)
 	engine.Run()
 }
