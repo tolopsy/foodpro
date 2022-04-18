2039dcc4c557e8d0c3b5ead3681ce4f8ae7831b8
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-04-18 01:25:07 +0100

    add db_provider to plug db into the platform

---
+++ provider/db_provider.go
@@ -0,0 +1,21 @@
+package provider
+
+import (
+	"github.com/tolopsy/foodpro/persistence"
+	"github.com/tolopsy/foodpro/persistence/mongolayer"
+)
+
+type DBTYPE string
+
+const (
+	MONGO_DB DBTYPE = "mongodb"
+)
+
+func NewDBHandler(dbType, dbURI, dbName string) (persistence.DatabaseHandler, error) {
+	switch DBTYPE(dbType) {
+	case MONGO_DB:
+		return mongolayer.NewMongoDBHandler(dbURI, dbName)
+	}
+
+	return nil, nil
+}
