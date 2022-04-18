dc0288f8f8f2a45587c5dd9f1e843144d163e075
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-04-18 01:21:55 +0100

    create recipe models in persistence package

---
+++ persistence/models.go
@@ -0,0 +1,14 @@
+package persistence
+
+import (
+	"time"
+)
+
+type Recipe struct {
+	ID           interface{} `json:"id,omitempty" bson:"_id,omitempty"`
+	Name         string      `json:"name,omitempty" bson:"name,omitempty"`
+	Tags         []string    `json:"tags,omitempty" bson:"tags,omitempty"`
+	Ingredients  []string    `json:"ingredients,omitempty" bson:"ingredients,omitempty"`
+	Instructions []string    `json:"instructions,omitempty" bson:"instructions,omitempty"`
+	PublishedAt  time.Time   `json:"publishedAt,omitempty" bson:"publishedAt,omitempty"`
+}
