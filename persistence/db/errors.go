216d698a2cbace099241e15d8b4f3593d2763f9c
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-04-29 08:50:36 +0100

    standardize db specific errors

---
+++ persistence/db/errors.go
@@ -0,0 +1,5 @@
+package db
+
+import "errors"
+
+var ErrorDBPluginDoesNotExist = errors.New("required database plugin does not exist")
--- provider/db_provider.go
+++ provider/db_provider.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"github.com/tolopsy/foodpro/persistence"
+	"github.com/tolopsy/foodpro/persistence/db"
 	"github.com/tolopsy/foodpro/persistence/db/mongolayer"
 )
 
@@ -15,7 +16,7 @@ func NewDBHandler(dbType, dbURI, dbName string) (persistence.DatabaseHandler, er
 	switch DBTYPE(dbType) {
 	case MONGO_DB:
 		return mongolayer.NewMongoDBHandler(dbURI, dbName)
+	default:
+		return nil, db.ErrorDBPluginDoesNotExist
 	}
-
-	return nil, nil
 }
