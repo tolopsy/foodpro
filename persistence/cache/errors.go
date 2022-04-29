ef361c043790f6195faa476109ec79a8b11fe7ab
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-04-29 08:53:09 +0100

    add error cache server plugin does not exist error

--- persistence/cache/errors.go
+++ persistence/cache/errors.go
@@ -2,4 +2,5 @@ package cache
 
 import "errors"
 
-var ErrorKeyDoesNotExist = errors.New("key does not exist")
+var ErrorKeyDoesNotExist = errors.New("key does not exist in cache")
+var ErrorCacheServerPluginDoesNotExist = errors.New("required cache server plugin does not exist")
