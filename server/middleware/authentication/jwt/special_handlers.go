f6172e7a25e528aaeca4b406c30272ed3706457e
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-05-22 12:20:11 +0100

    add jwt refresh method

---
+++ server/middleware/authentication/jwt/special_handlers.go
@@ -0,0 +1,52 @@
+// Handlers unique only to jwt. This handlers are not
+// necessarily part of the auth middleware methods.
+package jwt_auth
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func (jwtAuth *JWTAuth) Refresh(ctx *gin.Context) {
+	tokenValue := ctx.GetHeader(jwtAuth.headerKey)
+	claims := &Claims{}
+	getTokenSecret := func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtAuth.jwtSecret), nil
+	}
+	token, err := jwt.ParseWithClaims(tokenValue, claims, getTokenSecret)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Error while parsing token string -> " + err.Error()})
+		return
+	}
+
+	if token == nil || !token.Valid {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		return
+	}
+
+	expireBoundary := 30 * time.Second
+	expiryTime := time.Unix(claims.ExpiresAt, 0)
+	if time.Until(expiryTime) > expireBoundary {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Token has not expired"})
+		return
+	}
+
+	expiryTime = time.Now().Add(5 * time.Second)
+	claims.ExpiresAt = expiryTime.Unix()
+	token = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenValue, err = token.SignedString(jwtAuth.jwtSecret)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	jwtOutput := JWTOutput{
+		Token:   tokenValue,
+		Expires: expiryTime,
+	}
+
+	ctx.JSON(http.StatusOK, jwtOutput)
+}
