32875ca7cbb1c2b95b5f5f0a6650f1488745e7b0
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-05-22 13:53:45 +0100

    add user ops: verify user method

---
+++ persistence/db/mongolayer/user_ops.go
@@ -0,0 +1,17 @@
+package mongolayer
+
+import (
+	"crypto/sha256"
+
+	"github.com/tolopsy/foodpro/persistence"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func (db *DBHandler) VerifyUser(user persistence.User) bool {
+	h := sha256.New()
+
+	userCredentials := bson.M{"username": user.Username, "password": string(h.Sum([]byte(user.Password)))}
+	result := db.userCollection.FindOne(db.context, userCredentials)
+	
+	return result.Err() == nil
+}
